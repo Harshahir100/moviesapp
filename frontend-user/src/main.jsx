9d5c32054a8a9770e9c67d87d18ae0b43ea23aa8
harsh <harshad107@gmail.com>
2026-08-01 17:12:50 +0530

    Add SEO meta tags and improve website metadata

--- frontend-user/src/main.jsx
+++ frontend-user/src/main.jsx
@@ -1,11 +1,12 @@
 // src/main.jsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <HelmetProvider>
+    <App />
+  </HelmetProvider>,
 );
